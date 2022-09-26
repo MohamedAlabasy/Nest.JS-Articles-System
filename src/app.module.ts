77b205b86faf0464c6c423dbda39dcdca4049355
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-09-26 23:28:21 +0200

    add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    UsersModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      username: process.env.USER_NAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
       entities: [],
       synchronize: true,
+      autoLoadEntities: true,
     }),
+    UsersModule,
   ],
   // controllers: [],
   // providers: [],
