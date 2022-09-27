3746596280d784cea08a60f735ce117569d6563d
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-09-27 12:45:15 +0200

    add login token

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 
 import { CreateUsersDto } from './dto/create-users.dto';
 import { UsersService } from './users.service';
 import { RegisterPipe } from './pipes/register.pipe';
 import { LoginDto } from './dto/login.dto';
 import { LoginPipe } from './pipes/login.pipe';
+import { ACCESS_TOKEN_SECRET } from '../../config/token.config';
+
 
 
 
@@ -40,19 +43,25 @@ export class UsersController {
     @UsePipes(ValidationPipe)
     async login(@Body(LoginPipe) _userData: LoginDto) {
         try {
-            this.data = await this.usersService.login(_userData)
+            this.data = await this.usersService.login(_userData);
+
             if (!this.data) {
                 throw new Error(`there is no user with this email = ${_userData.email}`)
             }
-            let IsValidPassword = bcrypt.compareSync(_userData.password, this.data.password);
+
+            const IsValidPassword: boolean = bcrypt.compareSync(_userData.password, this.data.password);
             if (!IsValidPassword) {
                 throw new Error('invalid password')
-            } else {
-
             }
+
+            // to add token
+            const token: string = 'Bearer ' + jwt.sign({ id: this.data.id, email: this.data.email, is_verification: this.data.is_verification }, ACCESS_TOKEN_SECRET as string, {
+                expiresIn: 86400 //for 24 hour
+            });
 
             return {
                 statusCode: 200,
+                token,
                 data: this.data
             }
         } catch (error) {
