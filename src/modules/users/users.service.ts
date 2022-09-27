9387f6fafbe6f870c499e1ee0a1552092cdb1912
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-09-27 12:27:36 +0200

    add login pipe

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,30 +5,37 @@ import { Repository } from 'typeorm';
 
 import { Users } from 'src/database/entities/users.entity';
 import { CreateUsersDto } from './dto/create-users.dto';
+import { LoginDto } from './dto/login.dto';
+
+
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(Users) private usersRepository: Repository<Users>) { }
+    private data: any;
+    constructor(@InjectRepository(Users) private usersRepository: Repository<Users>) {
+        this.data = null;
+    }
 
     // #=======================================================================================#
     // #			                         create new user                                   #
     // #=======================================================================================#
-    async createNewUser(_user: CreateUsersDto): Promise<Users> {
-        const user = this.usersRepository.create({
-            name: _user.name,
-            email: _user.email,
+    async createNewUser(_userData: CreateUsersDto): Promise<Users> {
+        this.data = this.usersRepository.create({
+            name: _userData.name,
+            email: _userData.email,
+            password: _userData.password,
             is_verification: false,
-            password: _user.password,
             is_admin: false,
             token: null,
         });
-        return await this.usersRepository.save(user);
+        return await this.usersRepository.save(this.data);
     }
     // #=======================================================================================#
     // #			                            login                                          #
     // #=======================================================================================#
-    async login(_user: CreateUsersDto): Promise<Users> {
-        return await this.usersRepository.findOne({ where: { email: _user.email } });
+    async login(_userData: LoginDto): Promise<Users> {
+        return await this.usersRepository.findOne({ where: { email: _userData.email } });
     }
+
     // #=======================================================================================#
     // #                           get User by id for testing purpose                          #
     // #=======================================================================================#
