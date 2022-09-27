bd74740d90e2e8143580aaf93bf7165a12b3e4a3
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-09-27 13:07:17 +0200

    add check tokens middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 
 import { UsersModule } from './modules/users/users.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { CheckTokensMiddleware } from './middleware/check-tokens.middleware';
 
 @Module({
   imports: [
@@ -16,6 +17,11 @@ import { LoggerMiddleware } from './middleware/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*')
+    consumer
+      .apply(LoggerMiddleware).forRoutes('*')
+      .apply(CheckTokensMiddleware).exclude(
+        { path: 'users/login', method: RequestMethod.POST },
+        { path: 'users/register', method: RequestMethod.POST },
+      ).forRoutes('*')
   }
 }
